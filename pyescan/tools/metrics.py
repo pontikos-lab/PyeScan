160ff391b548767d9e19a7ebff463d34caf095cb
William Woof <w.woof@ucl.ac.uk>
2024-02-19 13:11:11 +0000

    Added support for enface distance metrics

--- pyescan/tools/metrics.py
+++ pyescan/tools/metrics.py
@@ -19,6 +19,7 @@ def get_mask_area(row):
     return area, horizontal_extent, vertical_extent
 
 def count_mask_clusters(row):
+    #Using watershet clustering
     import cv2
     import numpy as np
 
@@ -90,6 +91,12 @@ def get_circle_line_intersection(circle_centre, radius, line_start, line_end):
     return t1, t2
 
 def get_distance_mask(row, radius):
+    if row.modality == "OCT":
+        return get_distance_mask_oct(row, radius)
+    else:
+        return get_distance_mask_enface(row, radius)
+
+def get_distance_mask_oct(row, radius):
     import numpy as np
     bscan_start = 0, row.bscan_index * row.resolutions_mm_depth
     bscan_end = row.dimensions_mm_width, row.bscan_index * row.resolutions_mm_depth
@@ -109,7 +116,40 @@ def get_distance_mask(row, radius):
         mask[:,int(start*im_w):int(end*im_w)-1] = 1.0
     return mask
 
-def get_pixel_count_at_distance(row, radius, area_only=False):
+def get_distance_mask_enface(row, radius):
+    import numpy as np
+    from skimage.draw import disk
+
+    im_w = row.mask_width
+    im_h = row.mask_height
+    mask = np.zeros((im_h, im_w))
+    
+    radius_scale_factor = im_w / row.size_width
+    
+    if False:#radius_scale_factor != im_h / row.size_height: # different aspect ratio not supported
+        print(row.file_path)
+        print("Mask:", row.mask_width, row.mask_height)
+        print("Actual:", row.size_width, row.size_height)
+        #raise Exception()
+    
+    if not row.resolutions_mm_width: return 0
+    radius_scaled = radius / row.resolutions_mm_width #assuming the same in both dims
+    radius_scaled *= radius_scale_factor
+    
+    if 'fovea_x' in row:
+        fovea_location = row.fovea_x, row.fovea_y
+    else:
+        fovea_location = im_w // 2, im_h //2
+        
+    
+    center = fovea_location[1], fovea_location[0] # y, x (y=0 top)
+    shape = im_h, im_w # h, w
+    rr, cc = disk(center, radius_scaled, shape=shape)
+    mask[rr,cc] = 1.
+    
+    return mask
+
+def get_pixel_count_at_distance(row, radius, hexent_only=False):
     import numpy as np
     from PIL import Image
     
@@ -124,7 +164,7 @@ def get_pixel_count_at_distance(row, radius, area_only=False):
     if not mask.any():
         return 0. if return_single_value else [ 0. ] * len(radius)
     if mask.all():
-        val = row.pixel_count_horizontal if area_only else row.pixel_count
+        val = row.pixel_count_horizontal if hexent_only else row.pixel_count
         return val if return_single_value else [ val ] * len(radius)
 
     img = np.array(Image.open(row.file_path).convert('L')) > 0.
@@ -132,27 +172,34 @@ def get_pixel_count_at_distance(row, radius, area_only=False):
     results = list()
     for mask in masks:
         masked_img = img * mask
-        result = masked_img.any(axis=0).sum() if area_only else masked_img.sum()
+        result = masked_img.any(axis=0).sum() if hexent_only else masked_img.sum()
         results.append(result)
     return results[0] if return_single_value else results
 
 def get_distance_pixel_counts(row, distances=[.5, 1.5, 3.]):
-    return get_pixel_count_at_distance(row, distances, area_only=False)
+    return get_pixel_count_at_distance(row, distances, hextent_only=False)
 
 def get_distance_volumes(row, distances=[.5, 1.5, 3.]):
     scale_w = row.size_width / row.mask_width * row.resolutions_mm_width
     scale_h = row.size_height / row.mask_height * row.resolutions_mm_height
     scale = scale_w * scale_h * row.resolutions_mm_depth
     
-    results = get_pixel_count_at_distance(row, distances, area_only=False)
+    results = get_pixel_count_at_distance(row, distances, hextent_only=False)
     return [ r*scale for r in results ] 
 
 def get_distance_horizontal_pixel_counts(row, distances=[.5, 1.5, 3.]):
-    return get_pixel_count_at_distance(row, distances, area_only=True)
+    return get_pixel_count_at_distance(row, distances, hextent_only=True)
 
 def get_distance_areas(row, distances=[.5, 1.5, 3.]):
-    scale_w = row.size_width / row.mask_width * row.resolutions_mm_width
-    scale = scale_w * row.resolutions_mm_depth
+    hexent_only = (row.modality == "OCT")
     
-    results = get_pixel_count_at_distance(row, distances, area_only=True)
+    if hexent_only:
+        scale_w = row.size_width / row.mask_width * row.resolutions_mm_width
+        scale = scale_w * row.resolutions_mm_depth
+    else:
+        scale_w = row.size_width / row.mask_width * row.resolutions_mm_width
+        scale_h = row.size_height / row.mask_height * row.resolutions_mm_height
+        scale = scale_w * scale_h
+    
+    results = get_pixel_count_at_distance(row, distances, hexent_only=hexent_only)
     return [ r*scale for r in results ] 
